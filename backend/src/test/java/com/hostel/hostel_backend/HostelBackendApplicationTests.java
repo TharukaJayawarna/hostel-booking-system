52898b4ab40c853075961fe5ffb8824498a033d6
Tharuka Jayawarna <158444362+TharukaJayawarna@users.noreply.github.com>
2025-11-29 11:28:09 +0530

    feat: Initialized Spring Boot backend project

---
+++ backend/src/test/java/com/hostel/hostel_backend/HostelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostel.hostel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
